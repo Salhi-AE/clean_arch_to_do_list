cead55c7064480cda5a8ca02b2078e5b5d27f8a4
Salhi_AE <salhiabdennour58@gmail.com>
2026-01-11 17:47:43 +0100

    change TodoRepository (Temporary saving -> JSON File)

--- src/core/interfaces/repositories/TodoRepository.js
+++ src/core/interfaces/repositories/TodoRepository.js
@@ -1,35 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+
 class Todorepository {
   constructor() {
-    this.todos = [];
-    this.currentId = 1;
+    this.filePath = path.join(__dirname, "./todos.json");
+  }
+  async _readFile() {
+    try {
+      const data = await fs.readFile(this.filePath, "utf-8");
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+  }
+  async _saveFiles(todos) {
+    await fs.writeFile(this.filePath, JSON.stringify(todos, null, 2));
   }
   async persist(todo) {
-    todo.id = this.currentId++;
-    this.todos.push(todo);
-    return todo;
+    const todos = await this._readFiles();
+    todo.id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    todos.push(todo);
+    await this._saveFiles(todos);
+    return todos;
   }
-  async getAll() {
-    return this.todos;
-  }
-  async findById(id) {
-    return this.todos.find((todo) => todo.id === id);
-  }
-  // understand tomorrow إن شاء الله
-  async update(updatedTodo) {
-    const index = this.todos.findIndex((t) => t.id === updatedTodo.id);
-    if (index !== -1) {
-      this.todos[index] = updatedTodo;
-      return this.todos[index];
-    }
-    return null;
-  }
-  async delete(id) {
-    this.todos = this.todos.filter((todo) => todo.id != parseInt(id));
-    return true;
-  }
-  async deleteCompleted() {
-    this.todos = this.todos.filter((todo) => todo.completed === false);
-    return true;
+  async findAll() {
+    return await this._readFiles();
   }
 }
 export default Todorepository;
